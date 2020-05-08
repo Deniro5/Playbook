535deb75d4918b7c49c79ec6d5eb653e159cee57
USER <user@USERs-MacBook-Pro.local>
2020-05-08 15:53:08 -0400

    Updates

---
+++ precache-manifest.712053ffc10057be290d526cdc505310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb18ee34a082afa117ee7a553847a2c4",
+    "url": "/Playbook/index.html"
+  },
+  {
+    "revision": "d33080fa1dcfcd7ebe16",
+    "url": "/Playbook/static/css/main.d15c2629.chunk.css"
+  },
+  {
+    "revision": "c79c698bb891c8945db1",
+    "url": "/Playbook/static/js/2.cb5497ca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Playbook/static/js/2.cb5497ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33080fa1dcfcd7ebe16",
+    "url": "/Playbook/static/js/main.bcd5e4be.chunk.js"
+  },
+  {
+    "revision": "75342c7f349e1e4ab227",
+    "url": "/Playbook/static/js/runtime-main.c4aea92a.js"
+  }
+]);
